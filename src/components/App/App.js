a8c13207a0c75a3498cb4f3a4f02baff332eeb2f
Segolene Alquier <segolene.alquier@yahoo.com>
2020-03-26 15:02:22 +0000

    created SecureRoute component and tried it on profile page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 // import Signup from '../signup';
-import Login from '../login';
-import Register from '../register';
-// import Profile from '../profile';
+import Login from "../login";
+import Register from "../register";
+import Profile from "../profile";
 // import ProfileShow from '../profileshow';
 // import Search from '../search';
 // import Suggestions from '../suggestions';
 // import Visit from '../visit';
 // import Like from '../like';
-import Home from '../home';
-import { AuthProvider } from './AuthContext';
-// import SecureRoute from './SecureRoute';
+import Home from "../home";
+import { AuthProvider } from "./AuthContext";
+import SecureRoute from "./SecureRoute";
 // import NotLoggedRoute from './NotLoggedRoute';
-import 'react-toastify/dist/ReactToastify.css';
-import Nav from '../nav';
+import "react-toastify/dist/ReactToastify.css";
+import Nav from "../nav";
 // import Toaster from '../toaster';
 // import UserValidation from '../uservalidation';
 // import ResetForgotPassword from '../ResetforgotPassword';
@@ -36,7 +36,8 @@ function App() {
             {/* <NotLoggedRoute path="/login" component={Login} /> */}
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
-            {/* <NotLoggedRoute
+            {
+              /* <NotLoggedRoute
               path="/validation/newaccount/:token"
               component={UserValidation}
             />
@@ -47,12 +48,13 @@ function App() {
             />
             <SecureRoute path="/search" component={Search} />
             <SecureRoute path="/suggestions" component={Suggestions} />
-            <SecureRoute path="/profile/:username" component={ProfileShow} />
-            <SecureRoute path="/profile" component={Profile} />
-            <SecureRoute path="/visits" component={Visit} />
+            <SecureRoute path="/profile/:username" component={ProfileShow} />*/
+              <SecureRoute path="/profile" component={Profile} />
+              /*<SecureRoute path="/visits" component={Visit} />
             <SecureRoute path="/likes" component={Like} />
             <SecureRoute path="/chat" component={Chat} />
-            <SecureRoute path="/chatroom/:matchId" component={ChatRoom} /> */}
+            <SecureRoute path="/chatroom/:matchId" component={ChatRoom} /> */
+            }
             <Route path="/" component={Home} />
           </Switch>
         </Router>
---
+++ src/components/App/SecureRoute.js
@@ -0,0 +1,55 @@
+import React, { useState, useContext } from "react";
+import { Redirect, Route } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  progress: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}));
+
+const SecureRoute = ({ component: Component, ...rest }) => {
+  const classes = useStyles();
+  const { authContext } = useContext(AuthContext);
+  const [secureAuth, setSecureAuth] = useState(false);
+
+  authContext.userData.then(data => {
+    if (data === null || !data.success) {
+      window.location = "/login";
+    }
+    setSecureAuth(data.success);
+  });
+
+  if (!secureAuth) {
+    return (
+      <div className={classes.progress}>
+        <CircularProgress color="secondary" />
+      </div>
+    );
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (secureAuth) {
+          return <Component {...props} {...rest} />;
+        }
+        return <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+
+export default SecureRoute;
