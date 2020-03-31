a0d08dad21c137f3a4a1530000cd1dd8861b76c5
Yann <yann120@hotmail.fr>
2020-03-31 16:05:49 +0200

    findUser returns the same value name as in the DB

--- server/routes/findUser.js
+++ server/routes/findUser.js
@@ -3,10 +3,10 @@ const passport = require("passport");
 const findUser = (req, res) => {
   res.status(200).send({
     auth: true,
-    first_name: req.user.firstName,
-    last_name: req.user.lastName,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
     email: req.user.email,
-    username: req.user.userName,
+    userName: req.user.userName,
     picture: req.user.picture,
     language: req.user.language,
     message: "user found in db"
