cdb63fb6649233c1862ca30ae6e7efd04a03afac
Yann <yann120@hotmail.fr>
2020-03-31 16:53:24 +0200

    upload image button style done

--- src/components/profile/components/upperBoxProfile.js
+++ src/components/profile/components/upperBoxProfile.js
@@ -8,20 +8,52 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Fab from '@material-ui/core/Fab';
-import ReportProblemIcon from '@material-ui/icons/ReportProblem';
-import BlockIcon from '@material-ui/icons/Block';
+
 import LoggedDot from '../../profileshow/components/loggedDot';
+import CloudUpload from "@material-ui/icons/CloudUpload";
 
 const useStyles = makeStyles(theme => ({
   fabUpBox: {
-    margin: '0px 5px',
+    margin: "0px 5px"
+  },
+  container: {
+    position: "relative",
+    "&:hover": {
+      "& $profileImg": {
+        opacity: "0.3"
+      },
+      "& $middle": {
+        opacity: "1"
+      }
+    }
+  },
+  middle: {
+    transition: ".5s ease",
+    opacity: "0",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    msTransform: "translate(-50%, -50%)",
+    textAlign: "center"
   },
   profileImg: {
-    position: 'relative',
+    opacity: "1",
+    display: "block",
+    width: "100%",
+    height: "auto",
+    transition: ".5s ease",
+    backfaceVisibility: "hidden"
+  },
+  textImage: {
+    backgroundColor: "#f50057",
+    color: "white",
+    fontSize: "16px",
+    padding: "16px 32px"
   },
   blockedIcon: {
-    position: 'absolute',
-  },
+    position: "absolute"
+  }
 }));
 
 const UpperBoxProfile = ({
@@ -44,21 +76,27 @@ const UpperBoxProfile = ({
   };
 
   return (
-    <Box
-      className={classes.boxUpProfile}
-    >
+    <Box className={classes.boxUpProfile}>
       <Grid container className={classes.containerUpProfile} direction="row">
         <Grid item xs={12} sm={6} className={classes.containerUpProfileLeft}>
           <Grid item xs={6} sm={5}>
-            <img
-              className={upBoxClasses.profileImg}
-              src={
-                profile.picture ||
-                'https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png'
-              }
-              alt="My profile"
-              width="90%"
-            />
+            <div className={upBoxClasses.container}>
+              <img
+                className={upBoxClasses.profileImg}
+                src={
+                  profile.picture ||
+                  "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+                }
+                alt="My profile"
+                width="100%"
+              />
+              <div className={upBoxClasses.middle}>
+                <div class={upBoxClasses.textImage}>
+                  <CloudUpload/><br/>
+                  Upload Picture
+                </div>
+              </div>
+            </div>
           </Grid>
           <Grid
             item
@@ -70,7 +108,7 @@ const UpperBoxProfile = ({
               <span>{profile.firstname}</span>
             </div>
             <div>
-              {type === 'public' ? (
+              {type === "public" ? (
                 <LoggedDot
                   loggedState={profile.connected}
                   lastConnection={profile.lastConnection}
