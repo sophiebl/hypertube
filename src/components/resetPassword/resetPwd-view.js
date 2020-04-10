ec66db014b803b735a484359ed840cbd8a511103
Segolene Alquier <segolene.alquier@yahoo.com>
2020-04-10 15:22:16 +0000

    FRONT : created reset password form

--- src/components/resetPassword/resetPwd-view.js
+++ src/components/resetPassword/resetPwd-view.js
@@ -1,7 +1,97 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  submitFB: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#039be5",
+  },
+}));
 
 const ResetPassword = () => {
-  return <h1>Reset page</h1>;
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Change your password
+        </Typography>
+        <form className={classes.form} onSubmit={""}>
+          <TextField
+            fullWidth
+            margin="normal"
+            htmlFor="password1"
+            type="password"
+            name="password1"
+            onChange={""}
+            value={""}
+            id="password1"
+            variant="outlined"
+            required
+            autoFocus
+            label="New password"
+          />
+
+          <TextField
+            fullWidth
+            margin="normal"
+            htmlFor="password2"
+            type="password"
+            name="password2"
+            onChange={""}
+            value={""}
+            variant="outlined"
+            id="password2"
+            required
+            label="New password again"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            className={classes.submit}
+          >
+            Log In
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
 };
 
 export default ResetPassword;
