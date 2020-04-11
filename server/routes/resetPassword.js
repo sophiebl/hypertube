715f936ec3956094b40d636630ff13c2ca472953
Segolene Alquier <segolene.alquier@yahoo.com>
2020-04-11 13:26:31 +0000

    password now encrypted on reset

--- server/routes/resetPassword.js
+++ server/routes/resetPassword.js
@@ -1,5 +1,7 @@
+const bcrypt = require("bcrypt");
 const { sequelize } = require("../models/index");
 const User = sequelize.import("../models/user");
+const BCRYPT_SALT_ROUNDS = 12;
 
 module.exports = (app) => {
   app.post("/resetPassword", (req, res, next) => {
@@ -18,8 +20,12 @@ module.exports = (app) => {
               "We were unable to find a user for this token. Please sign up again 🙏",
           });
         } else {
-          user.password = newPassword;
-          user.save();
+          bcrypt
+            .hash(newPassword, BCRYPT_SALT_ROUNDS)
+            .then((hashedPassword) => {
+              user.password = hashedPassword;
+              user.save();
+            });
           res.status(200).send({
             resetDone: true,
             message:
