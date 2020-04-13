d02fc86f5078ec790adaf3c7b1546157f8d5213d
Yann <yann120@hotmail.fr>
2020-04-13 16:39:14 +0200

    front video player is ok

--- src/components/Player/player-view.js
+++ src/components/Player/player-view.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import PlayerContainer from './player-container'
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+  videoPlayer: {
+    maxWidth: "100vw"
+  },
+}));
 
 const Player = ({ movieRequest }) => {
-  const {} = PlayerContainer(movieRequest);
+  const { subs, player } = PlayerContainer(movieRequest);
+  const classes = useStyles();
+    const { torrentUrl, provider, quality, imdbId } = movieRequest;
+
   return (
     <>
-      <p>play the movie</p>
+      <p>
+        {imdbId} - {torrentUrl}- {provider}- {quality}
+      </p>
+      {torrentUrl && provider && quality && imdbId ? (
+        <video controls>
+          <source
+            src={`http://localhost:8080/api/player/stream?provider=${provider}&id=${imdbId}&magnet=${torrentUrl}&quality=${quality}`}
+            type="video/mp4"
+          />
+          {player && subs}
+        </video>
+      ) : null}
     </>
   );
 };
