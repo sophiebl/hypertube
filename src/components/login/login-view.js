e30e671c8e570797b3e53d69aa5e5d55ccd6570e
Yann <yann120@hotmail.fr>
2020-06-23 19:53:48 +0200

    changed link on register

--- src/components/login/login-view.js
+++ src/components/login/login-view.js
@@ -116,7 +116,7 @@ const Login = ( {location} ) => {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="/signup" variant="body2">
+              <Link href="/register" variant="body2">
                 Don't have an account? Sign Up
               </Link>
             </Grid>
