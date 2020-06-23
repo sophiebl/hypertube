05f2f606dcfcc96f07ba3a247394d147f44ebe21
Yann <yann120@hotmail.fr>
2020-06-23 20:27:04 +0200

    remove link import

--- src/components/MovieDetails/movie-view.js
+++ src/components/MovieDetails/movie-view.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Link } from "@material-ui/core";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
